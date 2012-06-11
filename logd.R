420a762c2d5c60ea025a7d68bdc3891486f287a0
Michael Kao <mkao006@gmail.com>
2012-06-11 09:32:10 +0200

    Modified for back estimating sigma_1^2 and reduced 1 parameter

--- logd.R
+++ logd.R
@@ -30,10 +30,111 @@ library(fGarch)
 ##                  appropriate consider our non-linear function.
 
 
+## logd.mgarch <- function(xt, beta, pt, which){
+##   ## xt <- as.numeric(xt)
+##   ## Initialise variables and list
+##   #k <- 20
+##   ## sigma1 <- attr(xt, "sigma1")
+##   sigma1 <- beta[4]
+##   T <- length(xt)
+##   lpt <- length(pt)
+##   lb <- length(beta)
+##   dl <- vector("list", length = 4)
+##   names(dl) <- c("ld", "db", "dt")
+
+##   ## Calculate the conditional variance
+##   ## sigma.t <- cond.sd(xt, beta)
+##   betaSum <- as.numeric(filter(xt[-T]^2, beta[2], "recursive"))
+##     sigma.t <-
+##         sqrt(c(sigma1^2,
+##                beta[1] * (1 - cumprod(rep.int(beta[2], T - 1)))/
+##                (1 - beta[2]) +
+##                cumprod(rep.int(beta[2], T - 1)) * sigma1^2 +
+##                beta[3] * betaSum))
+  
+##   ## Calculate the density
+##   if(which[1] == 1){
+##     dl$ld = log(2) - log(beta[5] + 1/beta[5]) -
+##       0.5 * log(2 * pi) - log(outer(sigma.t, pt)) -
+##       ((xt * beta[5])^2 * Heaviside(-xt) +
+##        (xt / beta[5])^2 * Heaviside(xt))/(2 * outer(sigma.t^2, pt^2))
+##   }
+
+##   ## Calculate the derivatives
+##   if(which[2] == 1){
+
+##     ## Piece wise Analytical derivative
+##     dldsigma = -1/sigma.t +
+##       ((xt * beta[5])^2 * Heaviside(-xt) +
+##        (xt / beta[5])^2 * Heaviside(xt))/outer(sigma.t^3, pt^2)
+
+##     convFilter <- 1:(T - 2) * cumprod(c(1, rep(beta[2], T - 3)))
+##     myconvolve <- beta[3] * convolve(convFilter, rev(xt^2),
+##                                      type = "open")[1:(T - 2)]
+##     cp.beta <- c(0, myconvolve)
+
+##     ## beta[1] - Mean of the conditional variance equation    
+##     dsigmadalpha0 <-
+##       c(-0.5 * ((sigma.t[2]^2 - beta[1] -
+##                  beta[2] * sigma.t[1]^2) * beta[3])^-0.5,
+##         ((1 - cumprod(rep.int(beta[2], T - 1))))/
+##         ((1 - beta[2])))
+##     ##plot(dsigmadalpha0)
+
+##     ## beta[2] - Coefficient for lagged variance
+##     dsigmadalpha1 <-
+##       c(-0.5 * sigma.t[1]^2 * ((sigma.t[2]^2 - beta[1] -
+##                  beta[2] * sigma.t[1]^2) * beta[3])^-0.5,
+##       ((beta[1] * cumsum(c(0, 1:(T - 2) * beta[2]^(0:(T - 3))))) +
+##         1:(T - 1) * beta[2]^(0:(T - 2)) * beta[4]^2 +
+##          cp.beta))
+
+##     ## plot(dsigmadalpha1)
+    
+##     ## beta[3] - Coefficient for lagged sqaured observation
+##     dsigmadbeta1 <- c(-0.5 * (sigma.t[2]^2 - beta[1] - beta[2] *
+##                               sigma.t[1]^2)^0.5 * beta[3]^-1.5, betaSum)
+
+##     ## beta[5] - The initial variance
+##     dsigmadsigma <-
+##       c(-beta[2] * sigma.t[1] * ((sigma.t[2]^2 - beta[1] -
+##                  beta[2] * sigma.t[1]^2) * beta[3])^-0.5,
+##         (2 * beta[4] * beta[2]^(1:(T - 1))))
+
+##     ## beta[5] - Skewness parameter
+##     dldxi = -((1 - 1/beta[5]^2)/(beta[5] + 1/beta[5])) + 
+##       (xt^2 / beta[5]^3 * Heaviside(xt) -
+##        xt^2 * beta[5] * Heaviside(-xt))/outer(sigma.t^2, pt^2)
+
+##     ## Combine everything into an array.
+##     ##
+##     ## TODO (Michael):Improve this part, it feels a little bit
+##     ##                clumsy. Also try to avoid abind.
+##     dldparams <- array(c(dldsigma * dsigmadalpha0/c(1, 2 * sigma.t[-1]),
+##                          dldsigma * dsigmadalpha1/c(1, 2 * sigma.t[-1]),
+##                          dldsigma * dsigmadbeta1/c(1, 2 * sigma.t[-1]),
+##                          dldsigma * dsigmadsigma/c(1, 2 * sigma.t[-1])),
+##                            dim = c(T, lpt, lb - 1))
+##     dl$db <- abind(dldparams, dldxi, along = 3)
+##   }
+##   if(which[3] == 1){
+##     dl$dt = -1/matrix(rep(pt, each = T), nc = lpt) +
+##       ((xt * beta[5])^2 * Heaviside(-xt) +
+##        (xt / beta[5])^2 * Heaviside(xt))/outer(sigma.t^2, pt^3)
+##   }
+##   dl
+## }
+
+## NOTE (Michael): Then the problem is that the xt is not outputed
+
 logd.mgarch <- function(xt, beta, pt, which){
   ## Initialise variables and list
-  xt <- as.numeric(xt)
-  #k <- 20
+  ## if(!is.null(attr(xt, "init.sigma")[2])){
+  ##   attr(xt, "init.sigma")[1] = as.numeric(sqrt(abs((s[2]^2 - beta[1] -
+  ##                           beta[2] * s[1]^2)/beta[3])))
+  ## }
+  ## sigma1 = attr(xt, "init.sigma")[1]
+  sigma1 = attr(xt, "sigma1")
   T <- length(xt)
   lpt <- length(pt)
   lb <- length(beta)
@@ -44,18 +145,19 @@ logd.mgarch <- function(xt, beta, pt, which){
   ## sigma.t <- cond.sd(xt, beta)
   betaSum <- as.numeric(filter(xt[-T]^2, beta[2], "recursive"))
     sigma.t <-
-        sqrt(c(beta[4]^2,
+        sqrt(c(sigma1^2,
                beta[1] * (1 - cumprod(rep.int(beta[2], T - 1)))/
                (1 - beta[2]) +
-               cumprod(rep.int(beta[2], T - 1)) * beta[4]^2 +
+               cumprod(rep.int(beta[2], T - 1)) * sigma1^2 +
                beta[3] * betaSum))
+  ## attr(xt, "init.sigma")[2] <- sigma.t[2]
   
   ## Calculate the density
   if(which[1] == 1){
-    dl$ld = log(2) - log(beta[5] + 1/beta[5]) -
+    dl$ld = log(2) - log(beta[4] + 1/beta[4]) -
       0.5 * log(2 * pi) - log(outer(sigma.t, pt)) -
-      ((xt * beta[5])^2 * Heaviside(-xt) +
-       (xt / beta[5])^2 * Heaviside(xt))/(2 * outer(sigma.t^2, pt^2))
+      ((xt * beta[4])^2 * Heaviside(-xt) +
+       (xt / beta[4])^2 * Heaviside(xt))/(2 * outer(sigma.t^2, pt^2))
   }
 
   ## Calculate the derivatives
@@ -63,52 +165,59 @@ logd.mgarch <- function(xt, beta, pt, which){
 
     ## Piece wise Analytical derivative
     dldsigma = -1/sigma.t +
-      ((xt * beta[5])^2 * Heaviside(-xt) +
-       (xt / beta[5])^2 * Heaviside(xt))/outer(sigma.t^3, pt^2)
+      ((xt * beta[4])^2 * Heaviside(-xt) +
+       (xt / beta[4])^2 * Heaviside(xt))/outer(sigma.t^3, pt^2)
 
-    sig.vec <- 2 * sigma.t
     convFilter <- 1:(T - 2) * cumprod(c(1, rep(beta[2], T - 3)))
     myconvolve <- beta[3] * convolve(convFilter, rev(xt^2),
                                      type = "open")[1:(T - 2)]
-    cp.beta <- c(0, 0, myconvolve)
+    cp.beta <- c(0, myconvolve)
 
     ## beta[1] - Mean of the conditional variance equation
+    ## d1 = -0.5 * ((sigma.t[2]^2 - beta[1] -
+    ##               beta[2] * sigma.t[1]^2) * beta[3])^-0.5
+    ##
+    d1 = -0.5 * ((sigma.t[2]^2 - beta[1] - beta[3] * xt[1]^2) * beta[2])^-0.5
+    d1 = 0
     dsigmadalpha0 <-
-      c(0, ((1 - cumprod(rep.int(beta[2], T - 1))))/(1 - beta[2]))
+      c(d1, ((1 - cumprod(rep.int(beta[2], T - 1))))/((1 - beta[2])))
 
     ## beta[2] - Coefficient for lagged variance
+    ## d2 = -0.5 * sigma.t[1]^2 * ((sigma.t[2]^2 - beta[1] -
+    ##                              beta[2] * sigma.t[1]^2) * beta[3])^-0.5
+    d2 = -0.5 * (sigma.t[2]^2 - beta[1] - beta[3] * xt[1]^2)^0.5 * beta[2]^-1.5
+    d2 = 0
     dsigmadalpha1 <-
-      (beta[1] * cumsum(c(0, 0, 1:(T - 2) * beta[2]^(0:(T - 3))))) +
-        c(0, (1:(T - 1)) * beta[2]^(0:(T - 2)) * beta[4]^2) +
-         cp.beta
-
+      c(d2, ((beta[1] * cumsum(c(0, 1:(T - 2) * beta[2]^(0:(T - 3))))) +
+        1:(T - 1) * beta[2]^(0:(T - 2)) * sigma1^2 + cp.beta))
+    
     ## beta[3] - Coefficient for lagged sqaured observation
-    dsigmadbeta1 <- c(0, betaSum)
+    ## d3 = -0.5 * (sigma.t[2]^2 - beta[1] - beta[2] *
+    ##   sigma.t[1]^2)^0.5 * beta[3]^-1.5
+    d3 = -0.5 * xt[1]^2 * ((sigma.t[2]^2 - beta[1] - beta[3] * xt[1]^2) *
+                           beta[2])^-0.5
+    d3 = 0
+    dsigmadbeta1 <- c(d3, betaSum)
 
-    ## beta[4] - The initial variance
-    dsigmadsigma <-
-      2 * beta[4] * beta[2]^(0:(T - 1))
-
-    ## beta[5] - Skewness parameter
-    dldxi = -((1 - 1/beta[5]^2)/(beta[5] + 1/beta[5])) +
-      (xt^2 / beta[5]^3 * Heaviside(xt) -
-       xt^2 * beta[5] * Heaviside(-xt))/outer(sigma.t^2, pt^2)
+    ## beta[4] - Skewness parameter
+    dldxi = -((1 - 1/beta[4]^2)/(beta[4] + 1/beta[4])) + 
+      (xt^2 / beta[4]^3 * Heaviside(xt) -
+       xt^2 * beta[4] * Heaviside(-xt))/outer(sigma.t^2, pt^2)
 
     ## Combine everything into an array.
     ##
     ## TODO (Michael):Improve this part, it feels a little bit
     ##                clumsy. Also try to avoid abind.
-    dldparams <- array(c(dldsigma * dsigmadalpha0/sig.vec,
-                         dldsigma * dsigmadalpha1/sig.vec,
-                         dldsigma * dsigmadbeta1/sig.vec,
-                         dldsigma * dsigmadsigma/sig.vec),
-                           dim = c(T, lpt, lb - 1))
+    dldparams <- array(c(dldsigma * dsigmadalpha0/c(1, 2 * sigma.t[-1]),
+                         dldsigma * dsigmadalpha1/c(1, 2 * sigma.t[-1]),
+                         dldsigma * dsigmadbeta1/c(1, 2 * sigma.t[-1])),
+                         dim = c(T, lpt, lb - 1))
     dl$db <- abind(dldparams, dldxi, along = 3)
   }
   if(which[3] == 1){
     dl$dt = -1/matrix(rep(pt, each = T), nc = lpt) +
-      ((xt * beta[5])^2 * Heaviside(-xt) +
-       (xt / beta[5])^2 * Heaviside(xt))/outer(sigma.t^2, pt^3)
+      ((xt * beta[4])^2 * Heaviside(-xt) +
+       (xt / beta[4])^2 * Heaviside(xt))/outer(sigma.t^2, pt^3)
   }
   dl
 }
@@ -116,14 +225,45 @@ logd.mgarch <- function(xt, beta, pt, which){
 ## Compile the function to increase the speed
 logd.mgarch <- cmpfun(logd.mgarch)
 
+## ## Test whether the parameters are valid
+## valid.mgarch <- function(x, beta, mix){
+##   beta[1] > 0 &&
+##   beta[2] >= 0 && beta[2] <= 1 &&
+##   beta[3] >= 0 && beta[3] <= 1 &&
+##   beta[4] > 0 &&
+##   beta[5] > 0
+## }
+
 ## Test whether the parameters are valid
 valid.mgarch <- function(x, beta, mix){
   beta[1] > 0 &&
   beta[2] >= 0 && beta[2] <= 1 &&
   beta[3] >= 0 && beta[3] <= 1 &&
-  beta[4] > 0 &&
-  beta[5] > 0
+  beta[4] > 0
 }
+
+## initial.mgarch <- function(x, beta = NULL, mix = NULL, kmax = NULL){
+##   if(is.null(beta)){
+##     gf <- try(garchFit(data = as.numeric(x), trace = FALSE,
+##                        include.mean = FALSE, cond.dist = "snorm"))
+##     if(!(inherits(gf, "try-error"))){
+##       cgf <- coef(gf)
+##       beta <- c(cgf["omega"], cgf["beta1"], cgf["alpha1"],
+##                 gf@sigma.t[1], cgf["skew"])
+##     } else {
+##       beta <- c(1e-6, 0.8, 0.1, sd(x), 1)
+##     }
+##   } else {
+##     beta = beta
+##   }
+##   names(beta) <- c("omega", "beta1", "alpha1", "sigma0", "xi")
+##   if(is.null(mix)){
+##     mix <- disc(1, 1)
+##   } else {
+##     mix = mix
+##   }
+##   list(beta = beta, mix = mix)
+## }
 
 initial.mgarch <- function(x, beta = NULL, mix = NULL, kmax = NULL){
   if(is.null(beta)){
@@ -131,15 +271,14 @@ initial.mgarch <- function(x, beta = NULL, mix = NULL, kmax = NULL){
                        include.mean = FALSE, cond.dist = "snorm"))
     if(!(inherits(gf, "try-error"))){
       cgf <- coef(gf)
-      beta <- c(cgf["omega"], cgf["beta1"], cgf["alpha1"],
-                gf@sigma.t[1], cgf["skew"])
+      beta <- c(cgf["omega"], cgf["beta1"], cgf["alpha1"], cgf["skew"])
     } else {
-      beta <- c(1e-6, 0.8, 0.1, sd(x), 1)
+      beta <- c(1e-6, 0.8, 0.1, 1)
     }
   } else {
     beta = beta
   }
-  names(beta) <- c("omega", "beta1", "alpha1", "sigma0", "xi")
+  names(beta) <- c("omega", "beta1", "alpha1", "xi")
   if(is.null(mix)){
     mix <- disc(1, 1)
   } else {
@@ -148,19 +287,18 @@ initial.mgarch <- function(x, beta = NULL, mix = NULL, kmax = NULL){
   list(beta = beta, mix = mix)
 }
 
-
 ## The weights function, this is useful when the data is discrete and
 ## there are duplicated.
 weights.mgarch <- function(x) 1
 
 ## Function to determine the grid
 gridpoints.mgarch <- function(x, beta, grid){
-  seq(1e-5, 20, length = grid)
+  seq(0.1, 20, length = grid)
 }
 
 ## Function to restrict the space of the support points
 suppspace.mgarch <- function(x, beta, mix){
-  c(1e-5, 20)
+  c(0.1, 20)
 }
 
 ## Function for converting different class of time series to mgarch
@@ -172,15 +310,28 @@ as.mgarch <- function(x){
     mgts <- as.numeric(data.matrix(x))
     class(mgts) <- "mgarch"
     attr(mgts, "sd") = sd(x)
+    attr(mgts, "sigma1") = abs(x[1])
+    ## attr(mgts, "sigma1") = sd(x)
     mgts
 }
 
 
-## summary result of the solution
+## ## summary result of the solution
+## summary.mgarch <- function(sol, digits = 4){
+##   cat(paste("Solution Standard Deviation: ",
+##             round(sqrt(sum(((sol$beta[5]^3 + 1/sol$beta[5]^3))/
+##                            (sol$beta[5] + 1/sol$beta[5]) *
+##                            sol$mix$pr * sol$mix$pt^2)), 4),
+##             "\n", sep = ""))
+##   cat(paste("Persistence: ", round(sum(sol$beta[2:3]), 4), "\n", sep = ""))
+##   cat(paste("Maximum gradient: ", round(max(abs(sol$grad)), 7), "\n", sep = ""))
+##   cat(paste("Log-likelihood: ", round(sol$ll, 4), "\n", sep = ""))
+## }
+
 summary.mgarch <- function(sol, digits = 4){
   cat(paste("Solution Standard Deviation: ",
-            round(sqrt(sum(((sol$beta[5]^3 + 1/sol$beta[5]^3))/
-                           (sol$beta[5] + 1/sol$beta[5]) *
+            round(sqrt(sum(((sol$beta[4]^3 + 1/sol$beta[4]^3))/
+                           (sol$beta[4] + 1/sol$beta[4]) *
                            sol$mix$pr * sol$mix$pt^2)), 4),
             "\n", sep = ""))
   cat(paste("Persistence: ", round(sum(sol$beta[2:3]), 4), "\n", sep = ""))
@@ -190,11 +341,11 @@ summary.mgarch <- function(sol, digits = 4){
 
 ## Extract coefficients
 coef.mgarch <- function(sol){
-  round(sol$beta, 7)
+  sol$beta
 }
 
-## Extract mixture
-mix.mgarch <- function(sol){
+## Extract mixture, need to define mix as a S3 generic function
+mix <- function(sol){
   sol$mix
 }
 
@@ -204,10 +355,10 @@ mix.mgarch <- function(sol){
 ##                 checks whether the log-likelihood is maximised by
 ##                 altering the coefficient and check the
 ##                 log-likelihood around its neighbourhood.
-gradCheck <- function(x, sol){
+gradCheck <- function(x, sol, tol = 1e-15){
   for(i in 1:length(sol$beta)){
     null <- rep(0, length(sol$beta))
-    null[i] <- 1e-5
+    null[i] <- tol
     max.est <- all(logLik.snpmle(as.mgarch(x), sol$beta, sol$mix)[1] >
                    logLik.snpmle(as.mgarch(x), sol$beta + null, sol$mix)[1],
                    logLik.snpmle(as.mgarch(x), sol$beta, sol$mix)[1] >
@@ -222,15 +373,29 @@ gradCheck <- function(x, sol){
 ##
 ## NOTES (Michael): Don't like the name of this function, will change it
 ##                  later.
+##
+## cond.sd <- function(x, beta){
+##   T <- length(x)
+##   betaSum <- as.numeric(filter(x[-T]^2, beta[2], "recursive"))
+##   sigma.t <-
+##     sqrt(c(beta[4]^2,
+##            beta[1] * ((1 - cumprod(rep.int(beta[2], T - 1))))/
+##            (1 - beta[2]) +
+##            cumprod(rep.int(beta[2], T - 1)) * beta[4]^2 +
+##            beta[3] * betaSum))
+##   sigma.t
+## }
+
+
 cond.sd <- function(x, beta){
+  sigma1 <- attr(x, "sigma1")
   T <- length(x)
   betaSum <- as.numeric(filter(x[-T]^2, beta[2], "recursive"))
   sigma.t <-
-    sqrt(c(beta[4]^2,
+    sqrt(c(sigma1^2,
            beta[1] * ((1 - cumprod(rep.int(beta[2], T - 1))))/
            (1 - beta[2]) +
-           cumprod(rep.int(beta[2], T - 1)) * beta[4]^2 +
+           cumprod(rep.int(beta[2], T - 1)) * sigma1^2 +
            beta[3] * betaSum))
   sigma.t
 }
-
